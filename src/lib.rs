845f86e035baa531ab08a6febe971c985f5256fe
tomyukn <5608467+tomyukn@users.noreply.github.com>
2021-04-29 17:17:14 +0900

    Apply table format for `tasks` output

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate prettytable;
+
 pub mod db;
 pub mod parser;
 pub mod subcommand;
--- src/subcommand/show_tasks.rs
+++ src/subcommand/show_tasks.rs
@@ -1,15 +1,30 @@
 use crate::db::Database;
 use anyhow::Result;
+use prettytable::{format, Table};
 use std::path::PathBuf;
 
 /// Remove a task name from the database.
 pub fn run(db_path: PathBuf) -> Result<()> {
     let db = Database::connect_r(&db_path)?;
-
     let tasks = db.get_tasknames()?;
-    for (num, task_name) in tasks {
-        println!("{0:>2}. {1}", num, task_name);
-    }
+    print_tasks(tasks);
 
     Ok(())
+}
+
+/// Print task names as a table format.
+fn print_tasks(tasks: Vec<(u32, String)>) {
+    let mut table = Table::new();
+    let table_format = format::FormatBuilder::new().padding(1, 1).build();
+    table.set_format(table_format);
+
+    // title
+    table.add_row(row![br -> "No", bl -> "Task"]);
+
+    // contents
+    for (num, task_name) in tasks {
+        table.add_row(row![r -> num, l -> task_name]);
+    }
+
+    table.printstd();
 }
