f04578a8842c66c59702bb7dd9e616db1f12f98f
tomyukn <5608467+tomyukn@users.noreply.github.com>
2021-04-30 17:38:07 +0900

    Refactor start subcommand

--- src/subcommand/start.rs
+++ src/subcommand/start.rs
@@ -5,51 +5,69 @@ use std::path::PathBuf;
 
 pub fn run(
     db_path: PathBuf,
-    task_number: Option<u32>,
+    taskname_number: Option<u32>,
     is_break_time: bool,
     time: Option<String>,
     break_taskname: &str,
 ) -> Result<()> {
-    // build start time
-    let start_time = match time {
-        Some(t) => TaskTime::parse_from_str_hhmm(&t)?,
-        None => TaskTime::now(),
-    };
+    let start_time = build_start_time(time, TaskTime::now())?;
 
     let mut db = Database::connect_rw(&db_path)?;
 
-    // fill end time of the previous task if it is empty
+    // end current task
     if let Some(current_task_id) = db.get_current_task_id()? {
-        let mut current_task = db.get_task(current_task_id)?;
-
-        let updated_task = current_task.set_end_time(Some(start_time));
-        db.update_task(current_task_id, &updated_task)?;
+        let updated_task = fill_end_time(&mut db, current_task_id, &start_time)?;
 
         println!(
             "{} ended at {}",
-            &updated_task.name(),
-            &start_time.to_string_hhmm()
+            updated_task.name(),
+            updated_task.end_time().unwrap().to_string_hhmm()
         );
     }
 
-    // create a new task
+    // start new task
     let new_task_name = if is_break_time {
-        Ok(String::from(break_taskname))
+        String::from(break_taskname)
     } else {
-        match task_number {
-            Some(id) => db.get_taskname(id),
+        match taskname_number {
+            Some(n) => db.get_taskname(n),
             None => Err(anyhow!("Task number was not provided")),
-        }
-    }?;
-
-    let new_task = Task::start(new_task_name.clone(), start_time);
-    db.add_task_entry(&new_task)?;
+        }?
+    };
+    let new_task = register_task(&mut db, new_task_name, start_time)?;
 
     println!(
         "{} started at {}",
-        new_task_name,
-        start_time.to_string_hhmm()
+        new_task.name(),
+        new_task.start_time().to_string_hhmm()
     );
 
     Ok(())
+}
+
+/// Build `TaskTime` form `HHMM` string.
+fn build_start_time(time: Option<String>, default: TaskTime) -> Result<TaskTime> {
+    let start_time = match time {
+        Some(t) => TaskTime::parse_from_str_hhmm(&t)?,
+        None => default,
+    };
+
+    Ok(start_time)
+}
+
+/// Fill the end time.
+fn fill_end_time(db: &mut Database, task_id: u32, end_time: &TaskTime) -> Result<Task> {
+    let mut task = db.get_task(task_id)?;
+    task.set_end_time(Some(*end_time));
+    db.update_task(task_id, &task)?;
+
+    Ok(task)
+}
+
+// start a task and register it to the database
+fn register_task(db: &mut Database, task_name: String, start_time: TaskTime) -> Result<Task> {
+    let new_task = Task::start(task_name, start_time);
+    db.add_task_entry(&new_task)?;
+
+    Ok(new_task)
 }
